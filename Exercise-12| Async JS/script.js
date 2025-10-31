7f92a8d3c98726113604faecb193512345acbe32
w3villa-rajveer-singh <rajveer.singh@w3villa.com>
2025-10-31 18:34:04 +0530

    Added Events file

--- Exercise-12| Async JS/script.js
+++ Exercise-12| Async JS/script.js
@@ -1,1 +1,23 @@
-setTimeout(() => console.log("first"), 6000);
+// Topic: Asynchronous JS
+
+// Q1. Create a function that logs a message after 2 seconds using setTimeout.
+
+setTimeout(() => {
+    console.log("Hello, World!");    // Message will be printed after 2 seconds..
+}, 2000);
+
+
+setInterval(() => {
+    console.log("Hi World!");        //Message will keep on printing after every 2 seconds..
+}, 2000);
+
+// Q2. Build a Promise that resolves after 1 second and logs a message.
+
+
+
+
+
+// Q3. Chain .then() and .catch() to handle resolved and rejected cases.
+
+
+
