dfc4ce8d841f5a26ad2095601207d176e3e9316a
w3villa-rajveer-singh <rajveer.singh@w3villa.com>
2025-11-01 17:36:58 +0530

    Added Async JS file

--- Exercise-12| Async JS/script.js
+++ Exercise-12| Async JS/script.js
@@ -7,17 +7,44 @@ setTimeout(() => {
 }, 2000);
 
 
-setInterval(() => {
-    console.log("Hi World!");        //Message will keep on printing after every 2 seconds..
-}, 2000);
 
 // Q2. Build a Promise that resolves after 1 second and logs a message.
 
-
-
-
-
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise will be resolved after 1 second!");
+    }, 1000);
+  });
+  
+  myPromise.then((message) => {
+    console.log(message);
+  });
+  
+  
 // Q3. Chain .then() and .catch() to handle resolved and rejected cases.
 
-
+const fetchData = new Promise((resolve, reject) => {
+    let success = true; // try changing it to false to test rejection
+  
+    setTimeout(() => {
+      if (success) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Failed to fetch data.");
+      }
+    }, 1000);
+  });
+  
+  // Handling promise
+  fetchData
+    .then((result) => {
+      console.log(result); // runs if resolved
+    })
+    .catch((error) => {
+      console.log(error); // runs if rejected
+    })
+    .finally(() => {
+      console.log("Operation completed.");
+    });
+  
 
